ac71fb8701a557407a71c36621e706517b7fb8a5
hibachihoe <43859732+hibachihoe@users.noreply.github.com>
2018-12-18 10:08:46 -0500

    Add files via upload

---
+++ problem1.py
@@ -0,0 +1,13 @@
+print('-' *60)
+print('Love Life Decision Bot:')
+print()
+answer = input('Are you emotionally available?') 
+if answer == 'yes': 
+	print('Go for it.')
+	print('Live your life.')
+	print('What is stopping you?')
+else:
+	print('I do not recommend you to get into a relationship.')
+	print('A better option:')
+	print('Take a year off of messy relationships. Start a new hobby like yoga.')
+print('-'*60)
---
+++ problem3.py
@@ -0,0 +1,12 @@
+print('-'*65)
+print('Bouncer Bot:')
+print()
+print('Welcome to Club425, the most lit club in downtown ACTvF. Before you can enter, I need you to answer some questions...')
+print()
+birth = input('What is your age?')
+if birth >= '21':
+	print('Welcome to Club425.')
+	print('*Unchains velvet rope*')
+else:
+	print('Too young, You should have just lied! Ha! Get off the premises.')
+print('-'*65)
